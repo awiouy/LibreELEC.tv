c4a67aecd107db864267067e8b0877bccacf2033
awiouy <awiouy@gmail.com>
2016-09-07 08:37:46 +0200

    libevent: clean up

--- packages/ELECtronLibre/libevent/package.mk
+++ packages/ELECtronLibre/libevent/package.mk
@@ -10,10 +10,10 @@ PKG_PRIORITY="optional"
 PKG_SECTION="tools"
 PKG_SHORTDESC="libevent: a library for asynchronous event notification"
 PKG_LONGDESC="The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached. It is meant to replace the asynchronous event loop found in event-driven network servers."
+PKG_AUTORECONF="yes"
 
 PKG_IS_ADDON="no"
 
-PKG_AUTORECONF="yes"
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --enable-openssl"
 
 post_makeinstall_target() {
