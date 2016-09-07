779f8ec626a63bdf19074c6c8e8821f52ef1b6c0
awiouy <awiouy@gmail.com>
2016-09-07 08:39:18 +0200

    sonarr_libmediainfo: clean up

--- packages/ELECtronLibre/sonarr_libmediainfo/package.mk
+++ packages/ELECtronLibre/sonarr_libmediainfo/package.mk
@@ -11,8 +11,9 @@ PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files"
 PKG_LONGDESC="MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files"
-PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_IS_ADDON="no"
 
 make_target() {
   cd Project/GNU/Library
