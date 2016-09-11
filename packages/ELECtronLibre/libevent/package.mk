d73eb48e958f98c9f36d35270eb415c56e96b87b
awiouy <awiouy@gmail.com>
2016-09-11 12:42:06 +0200

    libevent: simplify

--- packages/ELECtronLibre/libevent/package.mk
+++ packages/ELECtronLibre/libevent/package.mk
@@ -1,18 +1,10 @@
 PKG_NAME="libevent"
 PKG_VERSION="2.0.22-stable"
-PKG_REV="1"
-PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="http://libevent.org/"
 PKG_URL="https://github.com/libevent/libevent/releases/download/release-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libressl zlib"
-PKG_PRIORITY="optional"
-PKG_SECTION="tools"
 PKG_SHORTDESC="libevent: a library for asynchronous event notification"
-PKG_LONGDESC="The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached. It is meant to replace the asynchronous event loop found in event-driven network servers."
-PKG_AUTORECONF="yes"
-
-PKG_IS_ADDON="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static --enable-openssl"
 
