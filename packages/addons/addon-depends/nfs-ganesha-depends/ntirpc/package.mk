d3643f446449a220d0fdb2d8c80b0a2d19a13018
awiouy <awiouy@gmail.com>
2018-06-20 21:36:29 +0200

    ntirpc: initial package

---
+++ packages/addons/addon-depends/nfs-ganesha-depends/ntirpc/package.mk
@@ -0,0 +1,27 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="ntirpc"
+PKG_VERSION="1.6.2"
+PKG_SHA256="248366383232f6da27b1996bdd5c6eb876856ab5847e6976693f3c298f81d679"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="https://github.com/nfs-ganesha/ntirpc"
+PKG_URL="https://github.com/nfs-ganesha/ntirpc/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain krb5"
+PKG_LONGDESC="Transport-independent RPC"
