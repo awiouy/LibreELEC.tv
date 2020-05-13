fb27788c8226c7630719547519077d44f905bbb4
awiouy <awiouy@gmail.com>
2020-05-13 22:40:16 +0200

    aixlog: update to 1.3.0

--- packages/addons/addon-depends/snapcast-depends/aixlog/package.mk
+++ packages/addons/addon-depends/snapcast-depends/aixlog/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aixlog"
-PKG_VERSION="1.2.1"
-PKG_SHA256="3ed6f47b3658607edfd94faf98034d31906b0f04f3fe4355e87a16f8dd582384"
+PKG_VERSION="1.3.0"
+PKG_SHA256="748a00d3185eeb5cd8f0d751c4ef89d8c0948364024a432d2536eef883c9c782"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/badaix/aixlog"
 PKG_URL="https://github.com/badaix/aixlog/archive/v$PKG_VERSION.tar.gz"
