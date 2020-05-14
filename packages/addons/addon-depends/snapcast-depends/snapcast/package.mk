e1e8fb464b33cda19ca0e1167a1fdc8bea56f029
awiouy <awiouy@gmail.com>
2020-05-14 07:47:07 +0200

    snapcast: update to 0.19.0

--- packages/addons/addon-depends/snapcast-depends/snapcast/package.mk
+++ packages/addons/addon-depends/snapcast-depends/snapcast/package.mk
@@ -2,20 +2,19 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="snapcast"
-PKG_VERSION="0.15.0"
-PKG_SHA256="7c584fad4941a299339fe060174e33c4d810b1cbe80d6efbee54da3dafb252cc"
+PKG_VERSION="0.19.0"
+PKG_SHA256="15faf3727bed1a937df95e4fcb5c72412258833bb3eabfea32169436b695d9ba"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/badaix/snapcast"
 PKG_URL="https://github.com/badaix/snapcast/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain aixlog alsa-lib asio avahi flac libvorbis popl"
+PKG_DEPENDS_TARGET="toolchain aixlog alsa-lib asio avahi boost flac libvorbis popl opus soxr"
 PKG_LONGDESC="Synchronous multi-room audio player."
 PKG_TOOLCHAIN="make"
 
 pre_configure_target() {
   cd ..
   rm -rf .$TARGET_NAME
-  CXXFLAGS="$CXXFLAGS -pthread \
-                      -I$(get_build_dir aixlog)/include \
+  CXXFLAGS="$CXXFLAGS -I$(get_build_dir aixlog)/include \
                       -I$(get_build_dir asio)/asio/include \
                       -I$(get_build_dir popl)/include"
 }
